429f0de07fba917222b9d2cdc948bd03ab2fad31
Hossamkhatap31 <khatabhossam88@gmail.com>
2025-01-11 01:16:01 +0200

    frist

---
+++ js/index.js
@@ -0,0 +1,13 @@
+//anmaiton Aos libaray
+AOS.init();
+//anmaiton Aos libaray
+
+//start type scrpit 
+var typed = new Typed('#typed-js', {
+    strings: ['Hossam Khatap', 'Developer', 'Designer'],
+    typeSpeed: 150,
+    backSpeed: 50,
+    loop: true,
+    loopcount: Infinity,
+});
+// end type script
